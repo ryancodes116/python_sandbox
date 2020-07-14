c0036bfd69caa5e88e9af4044dea7631df639bd6
Ryan Moulton <ryanmoulton116@gmail.com>
2020-07-14 09:54:00 -0700

    add classes examples

--- classes.py
+++ classes.py
@@ -1,2 +1,44 @@
 # A class is like a blueprint for creating objects. An object has properties and methods(functions) associated with it. Almost everything in Python is an object
 
+# Create class
+class User:
+    # Constructor
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+
+    def greeting(self):
+        return f'My name is {self.name} and I am {self.age}'
+
+    def has_birthday(self):
+        self.age += 1
+
+
+# Extend class
+class Customer(User):
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+        self.balance = 0
+
+    def set_balance(self, balance):
+        self.balance = balance
+
+    def greeting(self):
+        return f'My name is {self.name} and I am {self.age} and my balance is {self.balance}'
+
+
+# Init user object
+ryan = User('Ryan', 'ryan@mail.com', 28)
+
+# Init customer
+janet = Customer('Janet', 'janet@mail.com', 30)
+
+ryan.has_birthday()
+print(ryan.greeting())
+
+print(janet.greeting())
+janet.set_balance(500)
+print(janet.balance)
